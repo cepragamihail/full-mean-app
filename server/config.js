7815bebacc3f6e56e4940ee1a25c089c55f35e47
mihail1986 <yahim09@gmail.com>
2019-05-10 22:46:21 +0300

    commit

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    AUTH0_DOMAIN: '[YOUR_AUTH0_DOMAIN]', // e.g., kmaida.auth0.com
-    AUTH0_API_AUDIENCE: '[YOUR_AUTH0_API_NAME]', // e.g., 'http://localhost:8083/api/'
-    MONGO_URI: 'mongodb://[USER]:[PASSWORD]@[DS######].mlab.com:[PORT]/[DB_NAME]'
+    AUTH0_DOMAIN: 'cepraga-mihail.eu.auth0.com', // '[YOUR_AUTH0_DOMAIN]', // e.g., kmaida.auth0.com
+    AUTH0_API_AUDIENCE: 'http://localhost:8083/api', // '[YOUR_AUTH0_API_NAME]', // e.g., 'http://localhost:8083/api/'
+    MONGO_URI: 'mongodb://mean:mean2019@ds343985.mlab.com:43985/polyglot' // 'mongodb://[USER]:[PASSWORD]@[DS######].mlab.com:[PORT]/[DB_NAME]'
   };
